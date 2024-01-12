de3f22f72ff18453a9f97df1277cb23bde1ff7ca
David Bauch <jamesbauch@gmail.com>
2024-01-12 14:30:33 -0700

    got things moved and working in intellij

---
+++ src/SimpleJavaClass.java
@@ -0,0 +1,15 @@
+public class SimpleJavaClass {
+	/*
+	private int x; // instance variable
+	private static boolean b = true; // class variable
+
+	static {
+		b = false; // sorta like a cpp constructor
+	}
+	*/
+
+	public static void main(String[] args) { // commandline args come as array of strings
+		System.out.println("Hello SimpleJavaClass.main()");
+	}
+}
+
