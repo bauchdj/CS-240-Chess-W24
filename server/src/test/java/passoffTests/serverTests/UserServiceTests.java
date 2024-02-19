ef11c77163b42423a6f082a23b9731b9c4f4bc94
David Bauch <jamesbauch@gmail.com>
2024-02-19 12:22:37 -0700

    Started UserServiceTests

---
+++ server/src/test/java/passoffTests/serverTests/UserServiceTests.java
@@ -0,0 +1,27 @@
+package passoffTests.serverTests;
+
+import org.junit.jupiter.api.*;
+import passoffTests.testClasses.TestException;
+
+import dataAccess.Database;
+import dataAccess.UserDAO;
+import dataAccess.AuthDAO;
+import service.UserService;
+import model.UserData;
+import model.AuthData;
+
+public class UserServiceTests {
+	private final Database db = new Database();
+	private final UserDAO userDAO = new UserDAO(db);
+	private final AuthDAO authDAO = new AuthDAO(db);
+	private final UserService userService = new UserService(userDAO, authDAO);
+
+	@Test
+	@Order(1)
+	@DisplayName("Register Success")
+	public void successRegister() throws TestException {
+		UserData userData = new UserData("user", "pwd", "user@chess.com");
+		userService.register(userData);
+		Assertions.assertEquals(userData, userDAO.getUser("user"));
+	}
+}
